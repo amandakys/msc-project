8cd67bf41b76fbc93aacedb33525c973e3ed5fc7
unknown <amandakys@gmail.com>
2022-11-17 15:36:54 +0000

    update index links, added profile to admin site

--- restaurant_review/admin.py
+++ restaurant_review/admin.py
@@ -1,7 +1,8 @@
 from django.contrib import admin
-from .models import Restaurant, Review
+from .models import Restaurant, Review, Profile
 
 # Register your models here.
 
 admin.site.register(Restaurant)
 admin.site.register(Review)
+admin.site.register(Profile)
